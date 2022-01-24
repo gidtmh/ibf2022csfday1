b0a5e1ca0a78d7ce6edd2cf5887063d364544528
gidtmh <gideontmh@gmail.com>
2022-01-24 15:12:52 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Com2Component } from './com2/com2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Com2Component
   ],
   imports: [
     BrowserModule
